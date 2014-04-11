312aea85cea8873f3bd9f6f3027203a63e439f16
Pzink3 <parker.zink@gmail.com>
2014-04-10 21:23:26 -0700

    Made Player Appear On Level Change
    Fixed an issue where the player wasn't appearing when we switched
    levels.

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -8,21 +8,25 @@ game.PlayScreen = me.ScreenObject.extend({
                 
                 me.levelDirector.loadLevel("level01");
                 
-                var player = new game.PlayerEntity(0, 420, {});
-                me.game.add(player, this.z);
+                this.resetPlayer();
 
-		
             // add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
 	},
 
-
-	/**
+         
+         /**
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
 		// remove the HUD from the game world
 		me.game.world.removeChild(this.HUD);
-	}
+	},
+
+        resetPlayer: function () {           
+                var player = new game.PlayerEntity(0, 420, {});
+                me.game.add(player, 3);
+        }
+
 });
